33158513d397f46bce862f5e136c98d75987c6e0
efrain-vasquez <efrain.vasquez.arias777@gmail.com>
2019-07-02 15:28:18 -0500

    update to schema

--- schema.sql
+++ schema.sql
@@ -6,9 +6,8 @@ USE test;
 
 CREATE TABLE items (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  address varchar (50) NOT NULL,
-  locationName varchar (50) NOT NULL,
-  description varchar (200) NOT NULL
+  address varchar 50 NOT NULL,
+  locationName varchar 50 NOT NULL,
+  description varchar 200 NOT NULL
 );
-INSERT INTO items (address, locationName, description) VALUES ('Toledo 39', 'Holacode', 'The coolest place on earth!
-
+INSERT INTO items (address, locationName, description) VALUES ('Toledo 39', 'Holacode', 'The coolest place on earth!')
